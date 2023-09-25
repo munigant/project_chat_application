7f63a738b557a46c9134d5ad595e809f000549be
Raju Galipelly <rajug@nurseio.com>
2023-09-25 07:40:04 +0530

    All files checked and changed

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -18,12 +18,14 @@ const TextContainer = ({ users }) => (
             <h1>People currently chatting:</h1>
             <div className="activeContainer">
               <h2>
-                {users.map(({name}) => (
+                {
+                users.map(({name}) => (
                   <div key={name} className="activeItem">
                     {name}
                     <img alt="Online Icon" src={onlineIcon}/>
                   </div>
-                ))}
+                ))
+                }
               </h2>
             </div>
           </div>
--- server/router.js
+++ server/router.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
+import { Router } from "express";
+const router = Router();
 
 router.get("/", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
 });
 
-module.exports = router;
+export default router;
